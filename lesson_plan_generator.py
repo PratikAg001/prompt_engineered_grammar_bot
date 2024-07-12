e8d67cde6914c2899fbbb69c0eade725552b9876
PratikAg001 <pratik@mysivi.ai>
2024-07-12 14:00:41 +0530

    ads

--- lesson_plan_generator.py
+++ lesson_plan_generator.py
@@ -1,17 +1,8 @@
 import json
-from langchain_openai import ChatOpenAI,AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from langchain.schema import SystemMessage
 import os
 
-# Function to generate subtopics
-# def generate_subtopics(topic, openai_api_key):
-#     chat = ChatOpenAI(
-#         openai_api_key=openai_api_key,
-#         model='gpt-3.5-turbo',
-#         temperature=0,
-#         max_tokens=None,
-#         timeout=None
-#     )
 def generate_subtopics(topic,api_key, api_endpoint):
     os.environ["AZURE_OPENAI_API_KEY"] = api_key
     os.environ["AZURE_OPENAI_ENDPOINT"] = api_endpoint
@@ -26,7 +17,10 @@ def generate_subtopics(topic,api_key, api_endpoint):
     messages = [
         SystemMessage(content=(
             f'''You are Sivi, an AI English Professor. Your task is to create subtopics on the topic {topic}
-            return a ***LIST*** format of the subtopics'''
+            return a ***LIST*** format of the subtopics
+            -Maximum should be 10 topics.
+            -You can combine 2 topics in one item if needed.
+            -Only give important and relevant topics'''
         ))
     ]
     
@@ -50,9 +44,11 @@ def generate_lesson_plan(subtopics_list, api_key,api_endpoint):
       SystemMessage(content=(
           f'''Generate a JSON object. The main key will be "subtopics".
             - Do NOT include triple backticks or any other formatting characters in the output.
-            Here is the subtopic_list : {subtopics_list}. Write an explanation and 2 engaging MCQs on each subtopic in a **JSON** format which will be used as a lesson plan later with the keys being topic, explanation, engaging_questions. Engaging questions will just have the keys - question and the options.'''
-      ))
+            Here is the subtopic_list : {subtopics_list}. Write an explanation and 2 engaging MCQs on each subtopic in a **JSON** format which will be used as a lesson plan later with the keys being topic, explanation, engaging_questions. Engaging questions will just have the keys - question and the options.
+            -The response should be a *JSON* object '''
+      ))  
     ]
     
     res = chat(messages)
+    print(res.content)
     return json.loads(res.content)['subtopics']
